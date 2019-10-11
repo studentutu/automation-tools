cce0afc0a07af232abc438071cdb063c5ecae31d
Kyle <rocktavious@icloud.com>
2019-10-11 15:07:11 -0500

    move script for pulling unity data to another file

---
+++ scripts/unity.sh
@@ -0,0 +1,3 @@
+export UNITY_JSON=$(curl -s "https://public-cdn.cloud.unity3d.com/hub/prod/releases-${UNITY_PLATFORM}.json")
+export UNITY_VERSION_JSON=$(echo $UNITY_JSON | jq -r --arg VERSION "$UNITY_VERSION" '.official[] | select( .version | contains($VERSION))')
+export UNITY_VERSION_FULL=$(echo $UNITY_VERSION_JSON | jq -r '.version')
