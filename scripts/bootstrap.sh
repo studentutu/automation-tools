8bb0d3e0ffe5fa03fc21005df9b3dd4185483c46
Kyle <rocktavious@icloud.com>
2019-10-11 15:26:54 -0500

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,3 @@
-
 mkdir -p ./scripts
 curl -sL -o ./scripts/docker.sh https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/docker.sh
 curl -sL -o ./scripts/install.sh https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/install.sh
@@ -10,27 +9,3 @@ curl -sL -o ./scripts/build-webgl.sh https://raw.githubusercontent.com/red-owl-g
 curl -sL -o ./scripts/run-tests.sh https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/run-tests.sh
 curl -sL -o ./scripts/export-package.sh https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/export-package.sh
 chmod a+x ./scripts/*.sh
-
-prerequisites()
-{
-  if (( $# != 1 ))
-  then
-    echo "Usage: prerequisites <platform>"
-    exit 1
-  fi
-  if [ "$1" == "linux" ]
-  then
-    apt-get update -y
-    apt-get install -y curl jq
-    apt-get clean
-    rm -rf /var/lib/apt/lists/*
-  elif [ "$1" == "darwin" ]
-  then
-    brew update
-    brew install curl jq
-  fi
-}
-
-export UNITY_PLATFORM=${1:-"linux"}
-export UNITY_VERSION=${2:-"2018.4"}
-prerequisites $UNITY_PLATFORM
