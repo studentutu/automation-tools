5f08ea9341e78d9a9aec8cf656ea43a24250a8a5
Kyle <rocktavious@icloud.com>
2019-10-11 16:32:55 -0500

    add some logging to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,12 +10,14 @@ install() {
   fi
   if [ "$1" == "linux" ]
   then
+    echo "[RedOwl] Downloading... $2"
     curl -so "DownloadedPackage.tar.xz" "$2"
     mkdir -p  "/opt/Unity"
     tar -C "/opt/Unity" -xf "DownloadedPackage.tar.xz"
     rm "DownloadedPackage.tar.xz"
   elif [ "$1" == "darwin" ]
   then
+    echo "[RedOwl] Downloading... $2"
     curl -so "DownloadedPackage.pkg" "$2"
     sudo installer -dumplog -package "DownloadedPackage.pkg" -target /
     rm "DownloadedPackage.pkg"
@@ -23,8 +25,10 @@ install() {
 }
 
 install_unity() {
+  echo "[RedOwl] Installing ${UNITY_VERSION_FULL} for ${UNITY_PLATFORM}."
   install $UNITY_PLATFORM $(echo $UNITY_VERSION_JSON | jq -r '.downloadUrl')
   for module in $UNITY_MODULES; do
+      echo "[RedOwl] Installing Unity Module ${module}."
       install $UNITY_PLATFORM $(echo $UNITY_VERSION_JSON | jq -r --arg MODULE "$module" '.modules[] | select( .id == $MODULE) | .downloadUrl')
   done
 }
