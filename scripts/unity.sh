c58f36b13b37e6926352daa2989bc397801c6cc2
Kyle <rocktavious@icloud.com>
2019-10-11 16:22:14 -0500

    Update unity.sh

--- scripts/unity.sh
+++ scripts/unity.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 export UNITY_JSON=$(curl -s "https://public-cdn.cloud.unity3d.com/hub/prod/releases-${UNITY_PLATFORM}.json")
 export UNITY_VERSION_JSON=$(echo $UNITY_JSON | jq -r --arg VERSION "$UNITY_VERSION" '.official[] | select( .version | contains($VERSION))')
 export UNITY_VERSION_FULL=$(echo $UNITY_VERSION_JSON | jq -r '.version')
