936858daf40ce3daf0453a1c76043bf2a9ac759e
Kyle <rocktavious@icloud.com>
2019-10-12 15:44:57 -0500

    Update build-webgl.sh

--- scripts/build-webgl.sh
+++ scripts/build-webgl.sh
@@ -1,9 +1,8 @@
 project=$1
-project_path=$(pwd)/
 
 error_code=0
 
-curl -sL -o ${project_path}Assets/RedOwlWebGLBuilder.cs https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/RedOwlWebGLBuilder.cs
+curl -sL -o $(pwd)Assets/RedOwlWebGLBuilder.cs https://raw.githubusercontent.com/red-owl-games/automation-tools/master/scripts/RedOwlWebGLBuilder.cs
 
 echo "[RedOwl] Building $project for WebGL."
 $Unity \
@@ -11,7 +10,7 @@ $Unity \
   -nographics \
   -silent-crashes \
   -logFile \
-  -projectPath "$project_path" \
+  -projectPath "$(pwd)" \
   -executeMethod RedOwlWebGLBuilder.build \
   -quit
 if [ $? = 0 ] ; then
