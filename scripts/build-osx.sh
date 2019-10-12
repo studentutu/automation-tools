9ffb50c7da4a7226edd419371cd0d56dcd610533
Kyle <rocktavious@icloud.com>
2019-10-12 14:26:08 -0500

    Update build-osx.sh

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,16 +1,13 @@
-project=$1
-project_path=$(pwd)/
-
 error_code=0
 
-echo "[RedOwl] Building $project for Mac OS."
+echo "[RedOwl] Building for Mac OS."
 $Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile \
-  -projectPath "$project_path" \
-  -buildOSX64Player "$(pwd)/build/osx/$project.app" \
+  -projectPath "$(pwd)" \
+  -buildOSX64Player "$(pwd)/build/osx/$1.app" \
   -quit
 if [ $? = 0 ] ; then
   echo "[RedOwl] Building Mac OS completed successfully."
