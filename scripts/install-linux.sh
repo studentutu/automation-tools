63f92de9a12bcf332558900f86d40c1c2909ee77
Kyle <rocktavious@icloud.com>
2019-10-12 23:03:01 -0500

    don't fail linux install after license activation request creation

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -4,4 +4,4 @@
 
 install_unity
 ln -s /opt/Unity/Editor/Unity /usr/bin/unity
-unity -quit -batchmode -nographics -logfile -createManualActivationFile
+unity -quit -batchmode -nographics -logfile -createManualActivationFile || true
