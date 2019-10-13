0dee2bef3c7c79773482408ff7e0d05f080c3371
Kyle <rocktavious@icloud.com>
2019-10-12 23:01:56 -0500

    fixup test runner

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,3 +1,4 @@
+test_results="$(pwd)/TestResults.xml"
 error_code=0
 
 echo "[RedOwl] Running unit tests."
@@ -7,19 +8,16 @@ $Unity \
   -silent-crashes \
   -logFile \
   -projectPath "$(pwd)" \
-  -editorTestsResultFile "$(pwd)/reports/unit-test-results.xml" \
   -runEditorTests \
-  -quit
+  -editorTestsResultFile "$test_results"
 
 if [ $? = 0 ] ; then
   echo "[RedOwl] Unit tests all passed successfully."
   error_code=0
 else
   echo "[RedOwl] Unit tests failed. Exited with $?."
+  cat $test_results
   error_code=1
 fi
-
-echo "[RedOwl] Test results:"
-cat $test_results
 
 exit $error_code
